0be00778c1d90001b99ac6345e1c883e204af835
Raynor <jackywilliam82@gmail.com>
2026-08-15 16:10:03 +0800

    chore: 升级本地候选版至 0.3.9.10

--- tools/collect-release.ps1
+++ tools/collect-release.ps1
@@ -6,7 +6,7 @@ param(
     [string] $OutputDirectory = "artifacts/release",
 
     [Parameter(Mandatory = $false)]
-    [string] $ExpectedAssemblyVersion = "0.3.9.9",
+    [string] $ExpectedAssemblyVersion = "0.3.9.10",
 
     [Parameter(Mandatory = $false)]
     [int] $ExpectedDalamudApiLevel = 15,
