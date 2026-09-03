89d89162d72ca1446f2cce3cc9d806037b140b35
Raynor <jackywilliam82@gmail.com>
2026-09-03 15:20:16 +0800

    chore: 更新0.4.0.0测试版本

--- tools/collect-release.ps1
+++ tools/collect-release.ps1
@@ -6,7 +6,7 @@ param(
     [string] $OutputDirectory = "artifacts/release",
 
     [Parameter(Mandatory = $false)]
-    [string] $ExpectedAssemblyVersion = "0.3.10.5",
+    [string] $ExpectedAssemblyVersion = "0.4.0.0",
 
     [Parameter(Mandatory = $false)]
     [int] $ExpectedDalamudApiLevel = 15,
